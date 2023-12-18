0eb3d0f369c9ae3009b9d3066492302e053bc56c
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2023-12-18 21:45:25 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+
+//express app
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+//routes 
+app.use('/api/workouts', workoutRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port ', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
